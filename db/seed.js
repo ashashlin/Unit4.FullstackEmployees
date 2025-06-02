6e5b32d182fb76876254c17fdc79ee30a9838d37
Ashley Lin <linashleyy@gmail.com>
2025-06-03 03:12:45 +0800

    Seed the employees table with 10 employees

--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,6 @@
+import { faker } from "@faker-js/faker";
 import db from "#db/client";
+import { createEmployee } from "#db/queries/employees";
 
 await db.connect();
 await seedEmployees();
@@ -6,5 +8,13 @@ await db.end();
 console.log("🌱 Database seeded.");
 
 async function seedEmployees() {
-  // TODO
+  for (let i = 0; i < 10; i++) {
+    const employee = {
+      name: faker.person.fullName(),
+      birthday: faker.date.birthdate({ mode: "age", min: 21, max: 70 }),
+      salary: faker.number.int({ min: 70000, max: 300000 }),
+    };
+
+    await createEmployee(employee);
+  }
 }
